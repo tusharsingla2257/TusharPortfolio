ff4448df28e3298ec0206488894ffc9e03a1fcbe
TusharX <tusharsingla2257@gmail.com>
2026-07-06 16:56:59 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+// let marks = 72
+
+// if(marks>=90){
+//     console.log("A Grade")
+// }
+// else if(marks<= 90 && marks >= 75){
+//     console.log("B Grade")
+// }
+// else if(marks<= 75 && marks >= 60){
+//     console.log("C Grade")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// let a = 25;
+
+// if(a%5 == 0){
+//     console.log("Yes")
+// }
+// else{
+//     console.log("No")
+// }
+
+// let a = -46
+
+// if(a>0){
+//     console.log("Positive")
+// }
+// else if(a<0){
+//     console.log("Negative")
+// }
+// else{
+//     console.log("Zero")
+// }
+
+// let a=5;
+// b = a*a;
+
+// console.log(b)
+
+// let a = 25;
+// b = a%4;
+
+// console.log(b)
+
+
+let password = "javascript123";
+
+if(password = "javascript123"){
+    console.log("Welcome")
+}
+else{
+    console.log("Wrong Password")
+}
